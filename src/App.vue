9c71b1314a003bda23c297e0f7a138eca4fd429f
Samundra Dhakal <samundra.dhakal@tuni.fi>
2022-09-06 15:53:43 +0300

    add component in the app

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <h1>Samundra Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block_item v-if="isPlaying" :delay="delay"/>
+  <Block_item v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results_item v-if="showResult" :score="score"/>
 </template>
 
 <script>
 import Block_item from "./components/Block_item.vue"
+import Results_item from "./components/Results_item.vue"
 export default {
   name: 'App',
-  components: {Block_item},
+  components: {Block_item, Results_item},
   data(){
     return {
       isPlaying: false,
-      delay:null
+      delay:null,
+      score: null,
+      showResult: false
     }
   },
   methods: {
     start(){
       this.delay=2000 + Math.random()*5000
       this.isPlaying=true
+      this.showResult = false
+    },
+    endGame(value) {
+      this.score = value
+      this.isPlaying = false
+      this.showResult = true
     }
   }
 }
@@ -32,5 +42,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
